866b15229e8e363500e64ebbf7b2a2abed979b8b
aliceoq <aoqb@cin.ufpe.br>
2023-12-18 23:07:54 -0300

    fix(front): detect language

--- front/src/i18n.ts
+++ front/src/i18n.ts
@@ -1,21 +1,25 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-import enTranslation from './locales/en.json';
-import ptTranslation from './locales/pt.json';
+import enTranslation from "./locales/en.json";
+import ptTranslation from "./locales/pt.json";
 
 const resources = {
   en: { translation: enTranslation },
   pt: { translation: ptTranslation },
 };
 
-i18n.use(initReactI18next).init({
-  resources,
-  lng: 'pt',
-  keySeparator: false,
-  interpolation: {
-    escapeValue: false,
-  },
-});
+i18n
+  .use(LanguageDetector)
+  .use(initReactI18next)
+  .init({
+    debug: true,
+    fallbackLng: "en",
+    interpolation: {
+      escapeValue: false,
+    },
+    resources,
+  });
 
 export default i18n;

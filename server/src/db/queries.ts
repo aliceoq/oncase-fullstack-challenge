5fa212d877b81be94b8cccfbad70ac8e26662d0a
aliceoq <aoqb@cin.ufpe.br>
2023-12-19 17:40:20 -0300

    fix(server): sort participants

--- server/src/db/queries.ts
+++ server/src/db/queries.ts
@@ -10,7 +10,7 @@ const createParticipantQuery = async (participant: Participant) => {
 };
 
 const getParticipantsQuery = async (): Promise<Participant[]> => {
-  const { rows } = await pool.query('SELECT * FROM "participants"');
+  const { rows } = await pool.query('SELECT * FROM "participants" ORDER BY id ASC');
   return rows;
 };
 
